1506e44ef7715b780ed1a575fbbaa9d64856e65c
Rayn <greenbellpaper13@gmail.com>
2026-02-07 18:54:57 -0500

    fix1

--- Core.py
+++ Core.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0adfc1aa5c6de06f7073b222cd99e008dd9d799642c51d77d5dcd43a749250c
-size 74935
+oid sha256:e7cbb6135bdfce7e960a85b5b89d417164ec5fb18b109fbb844f1367ccdf5c4d
+size 74676
